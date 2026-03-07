60e69b55619427eded858201ab01e0b0d9d27e7a
Koonavoot Kaewnopparat <169038824+Koonavoot@users.noreply.github.com>
2026-03-07 15:34:35 +0700

    Remove allowedDevOrigins and fix string interpolation

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  allowedDevOrigins: ['tonnam.pskwr.com'],
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: '${process.env.NEXT_PUBLIC_API_URL}/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
 };
-
 module.exports = nextConfig;

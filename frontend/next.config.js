f08dd9e871201ce40c25c71dbffbcf063719af2b
Koonavoot Kaewnopparat <169038824+Koonavoot@users.noreply.github.com>
2026-03-07 15:17:08 +0700

    Use environment variable for API URL in rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://tonnam-api.pskwr.com/:path*',
+        destination: '${process.env.NEXT_PUBLIC_API_URL}/:path*',
       },
     ];
   },
